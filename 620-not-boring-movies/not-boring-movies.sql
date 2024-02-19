6a94396697ba5c3f346b527022ef3dba97ebadbd
Rashika Suresh <rashikasuresh5758@gmail.com>
2024-02-20 05:23:17 +0530

    Time: 409 ms (74.41%) | Memory: 0B (100.00%) - LeetSync

--- 620-not-boring-movies/not-boring-movies.sql
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -1,3 +1,18 @@
 # Write your MySQL query statement below
 
-select * from cinema where id % 2 <> 0 and description <> "boring" order by rating desc;
+-- select * from cinema where id % 2 <> 0 and description <> "boring" order by rating desc;
+
+
+/* Write your PL/SQL query statement below */
+-- SELECT * FROM Cinema WHERE MOD( id, 2) = 1 AND 
+
+-- description <> 'boring' ORDER BY rating DESC
+
+-- select * from cinema c
+-- where c.id mod 2 > 0 and c.description not in ('boring')
+-- order by c.rating desc
+
+
+select id,movie ,description,rating from cinema WHERE id%2 <> 0 AND description NOT LIKE 'boring'
+ORDER BY rating DESC;
+
