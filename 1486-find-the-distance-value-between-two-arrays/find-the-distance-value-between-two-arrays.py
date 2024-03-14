db71ed354b6337862265b15a66b0b471aab14a45
Rashika Suresh <rashikasuresh5758@gmail.com>
2024-03-14 06:31:32 +0530

    Time: 72 ms (76.40%) | Memory: 16.8 MB (36.64%) - LeetSync

--- 1486-find-the-distance-value-between-two-arrays/find-the-distance-value-between-two-arrays.py
+++ 1486-find-the-distance-value-between-two-arrays/find-the-distance-value-between-two-arrays.py
@@ -1,10 +1,24 @@
 class Solution:
     def findTheDistanceValue(self, arr1: List[int], arr2: List[int], d: int) -> int:
+        # count = 0
+        # for i in arr1:
+        #     for j in arr2:
+        #         if abs(i-j) <= d:
+        #             count+=1
+        #             break
+        # return len(arr1)-count
         count = 0
-        for i in arr1:
-            for j in arr2:
-                if abs(i-j) <= d:
-                    count+=1
+        arr2.sort()
+        for i in range(len(arr1)):
+            left, right = 0, len(arr2) - 1
+            while left <= right:
+                mid = (left+right)//2
+                if abs(arr1[i]-arr2[mid])<=d:
+                    count += 1
                     break
-        return len(arr1)-count
+                elif arr1[i] < arr2[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+        return len(arr1) - count
         
