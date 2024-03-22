e7b376605cd3ea993d5aa138cf57cb26b67ae14a
Rashika Suresh <rashikasuresh5758@gmail.com>
2024-03-22 07:35:55 +0530

    Time: 39 ms (44.12%) | Memory: 16.7 MB (10.24%) - LeetSync

---
+++ 202-happy-number/happy-number.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+
+        slow = self.squared(n)
+        fast = self.squared(self.squared(n))
+
+        while slow != fast and fast!= 1:
+            slow = self.squared(slow)
+            fast = self.squared(self.squared(fast))
+
+        return fast == 1
+
+
+    
+    def squared(self, n):
+        res = 0
+        while n > 0:
+            last = n % 10
+            res += last * last
+            n = n // 10
+        
+        return res
+        
